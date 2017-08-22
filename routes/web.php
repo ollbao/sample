e0db9e451f4c81ccc5f4f59468053ab1f283c166
zhongjinbao <zhong1714@qq.com>
2017-08-22 12:00:53 +0800

    6.1用户注册完成

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('/signup','UsersController@create')->name('signup');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');//单个用户页面
+Route::get('/users/create', 'UsersController@create')->name('users.create');//创建页面
+Route::post('/users/store', 'UsersController@store')->name('users.store');//创建页面提交
+
